98882303fdcc97e1e077bf3adbec7dce0160c5e1
samuele-riva <s.riva72@campus.unimib.it>
2019-12-03 10:59:12 +0100

    Correzione pitfall: "Rename "test" which hides the field declared at line 75." test -> testS

--- src/main/java/org/junit/internal/runners/JUnit38ClassRunner.java
+++ src/main/java/org/junit/internal/runners/JUnit38ClassRunner.java
@@ -155,9 +155,9 @@ public class JUnit38ClassRunner extends Runner implements Filterable, Orderable 
             TestSuite filtered = new TestSuite(suite.getName());
             int n = suite.testCount();
             for (int i = 0; i < n; i++) {
-                Test test = suite.testAt(i);
-                if (filter.shouldRun(makeDescription(test))) {
-                    filtered.addTest(test);
+                Test testS = suite.testAt(i);
+                if (filter.shouldRun(makeDescription(testS))) {
+                    filtered.addTest(testS);
                 }
             }
             setTest(filtered);
